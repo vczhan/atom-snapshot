d886e98a97d7dbd10f1e0c51d7a456d3fe588096
admin <Admin@codeMacBook-Pro.local>
2017-01-12 04:02:49 +0800

    create atom-snapshot

---
+++ spec/snapshot-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SnapshotView from '../lib/snapshot-view';
+
+describe('SnapshotView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
