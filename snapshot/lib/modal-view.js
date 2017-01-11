ae37ec0a27cab678dc86a0a4e8795d90107db23f
admin <Admin@codeMacBook-Pro.local>
2017-01-12 03:58:53 +0800

    create atom-snapshot

---
+++ snapshot/lib/modal-view.js
@@ -0,0 +1,85 @@
+'use babel';
+
+import {$, View, TextEditorView } from 'atom-space-pen-views'
+
+export default class modalView extends View {
+
+  static content() {
+    return this.div({
+      class: 'modal-view'
+    }, () => {
+      this.h1('Create a new snapshot')
+      this.div({
+        class: 'inset-panel padded'
+      }, () => {
+        this.label('Set a name:', {
+          class: 'pv-label'
+        })
+        this.subview('name', new TextEditorView({
+          mini: true,
+          placeholderText: '未命名'
+        }))
+      })
+      this.div({
+        class: 'inset-panel padded'
+      }, () => {
+        this.label('Enter the describe:', {
+          class: 'pv-label'
+        })
+        this.subview('desc', new TextEditorView({
+          placeholderText: ''
+        }))
+      })
+      this.div({
+        class: 'block padded',
+        style: 'text-align:right'
+      }, () => {
+        this.button('新建', {
+          class: 'inline-block btn btn-success',
+          click: 'create'
+        })
+        this.button('取消', {
+          class: 'inline-block btn',
+          click: 'closeModal'
+        })
+      })
+    })
+  }
+
+  initialize(snapshot) {
+    this.snapshot = snapshot
+
+    if (this.panel == null) {
+      this.panel = atom.workspace.addModalPanel({
+        item: this
+      })
+    }
+
+    this.panel.show()
+  }
+
+  create(e) {
+    e.stopPropagation()
+    e.preventDefault()
+
+
+
+    this.snapshot.addSnapshotSuccess({
+      name: this.name.getText() || '未命名',
+      desc: this.desc.getText() || '无'
+    })
+
+    this.destroy()
+  }
+
+  closeModal(e) {
+    e.stopPropagation()
+    e.preventDefault()
+
+    this.destroy()
+  }
+
+  destroy() {
+    atom.workspace.panelForItem(this).destroy()
+  }
+}
